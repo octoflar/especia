3dbfa346406abb6214c38abbab9b1c06f4dff457
octoflar <octoflar@icloud.com>
2017-01-29 19:44:12 +0100

    update

--- src/main/cxx/model.h
+++ src/main/cxx/model.h
@@ -390,7 +390,7 @@ namespace especia {
             os << "  Created by\n";
             os << "  Evolutionary spectrum inversion and analysis (Especia)\n";
             os << "  " << PROJECT_LONG_NAME << " " << DOI << "\n";
-            os << "  " << SYSTEM << " " << SYSTEM_PROCESSOR << "\n";
+            os << "  " << SYSTEM << " " << "\n";
             os << "-->\n";
             os << "<!--\n";
             os << "<data>\n";
@@ -599,7 +599,7 @@ namespace especia {
             os << "  Created by\n";
             os << "  Evolutionary spectrum inversion and analysis (Especia)\n";
             os << "  " << PROJECT_LONG_NAME << " " << DOI << "\n";
-            os << "  " << SYSTEM << " " << SYSTEM_PROCESSOR << "\n";
+            os << "  " << SYSTEM << " " << "\n";
             os << "-->\n";
 
             if (trace > 0) {
