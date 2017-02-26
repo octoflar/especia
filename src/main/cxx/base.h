f0f4fd9ca205465ebf23203dfa3987e3462f17de
octoflar <octoflar@icloud.com>
2017-02-26 13:04:26 +0100

    API doc

--- src/main/cxx/base.h
+++ src/main/cxx/base.h
@@ -176,23 +176,21 @@ namespace especia {
     /**
      * Solves the equation f(x) = c by means of Newton's method.
      *
-     * @param f[in] A function, which takes as arguments:
+     * @param f[in] A differentiable function, which takes as parameters:
      * @parblock
-     * @c x An abscissa value
-     *
-     * @c y The result y = f(x).
-     *
-     * @c z The derivative of @c y with respect to @c x.
+     * @param x The abscissa value
+     * @param y The result y = f(x).
+     * @param z The derivative of @c y with respect to @c x.
      * @endparblock
-     * @param c[in] A constant.
-     * @param x[in] An initial guess of the solution.
+     * @param c[in] The constant on the right-hand side of the equation.
+     * @param x[in] The initial guess of the solution.
      * @param accuracy_goal[in] The accuracy goal (optional).
      * @param max_iteration[in] The maximum number of iterations (optional).
      *
      * @return the solution to the equation f(x) = c.
      */
     inline
-    double solve(void f(const double &, double &, double &), double c, double x, double accuracy_goal = 1.0E-6,
+    double solve(void f(const double &x, double &y, double &z), double c, double x, double accuracy_goal = 1.0E-6,
                  unsigned int max_iteration = 100) throw(std::runtime_error) {
         using std::abs;
         using std::runtime_error;
