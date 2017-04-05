67db7ed117767840b6ce7284a377a5827176baa1
octoflar <octoflar@icloud.com>
2017-04-05 09:11:42 +0200

    Updated tests with initial values and stop generation numbers from literature

--- src/test/cxx/core/optimizer_test.cxx
+++ src/test/cxx/core/optimizer_test.cxx
@@ -31,90 +31,6 @@ using especia::Optimizer;
 class Optimizer_Test : public Unit_Test {
 private:
 
-    void before_all() {
-        builder.with_problem_dimension(10).
-                with_population_size(8).
-                with_parent_number(4).
-                with_accuracy_goal(1.0E-06).
-                with_stop_generation(2000).
-                with_random_seed(31415);
-    }
-
-    void test_minimize_sphere() {
-        using especia::No_Constraint;
-        using especia::No_Tracing;
-
-        const valarray<R_type> x(1.0, 10);
-        const valarray<R_type> d(1.0, 10);
-        const R_type s = 1.0;
-
-        const Optimizer optimizer = builder.build();
-        const Optimizer::Result result = optimizer.minimize(sphere, x, d, s, No_Constraint<>(), No_Tracing<>());
-
-        assert_true(result.is_optimized(), "test minimize sphere: optimized");
-        assert_equals(0.0, result.get_fitness(), 1.0E-10, "test minimize sphere: fitness");
-        assert_equals(0.0, result.get_parameter_values()[0], 1.0E-06, "test minimize sphere: parameter 0");
-        assert_equals(0.0, result.get_parameter_values()[1], 1.0E-06, "test minimize sphere: parameter 1");
-        assert_equals(0.0, result.get_parameter_values()[2], 1.0E-06, "test minimize sphere: parameter 2");
-        assert_equals(0.0, result.get_parameter_values()[3], 1.0E-06, "test minimize sphere: parameter 3");
-        assert_equals(0.0, result.get_parameter_values()[4], 1.0E-06, "test minimize sphere: parameter 4");
-        assert_equals(0.0, result.get_parameter_values()[5], 1.0E-06, "test minimize sphere: parameter 5");
-        assert_equals(0.0, result.get_parameter_values()[6], 1.0E-06, "test minimize sphere: parameter 6");
-        assert_equals(0.0, result.get_parameter_values()[7], 1.0E-06, "test minimize sphere: parameter 7");
-        assert_equals(0.0, result.get_parameter_values()[8], 1.0E-06, "test minimize sphere: parameter 8");
-        assert_equals(0.0, result.get_parameter_values()[9], 1.0E-06, "test minimize sphere: parameter 9");
-    }
-
-    void test_minimize_cigar() {
-        using especia::No_Constraint;
-        using especia::No_Tracing;
-
-        const valarray<R_type> x(1.0, 10);
-        const valarray<R_type> d(1.0, 10);
-        const R_type s = 1.0;
-
-        const Optimizer optimizer = builder.build();
-        const Optimizer::Result result = optimizer.minimize(cigar, x, d, s, No_Constraint<>(), No_Tracing<>());
-
-        assert_true(result.is_optimized(), "test minimize cigar: optimized");
-        assert_equals(0.0, result.get_fitness(), 1.0E-10, "test minimize cigar: fitness");
-        assert_equals(0.0, result.get_parameter_values()[0], 1.0E-06, "test minimize cigar: parameter 0");
-        assert_equals(0.0, result.get_parameter_values()[1], 1.0E-06, "test minimize cigar: parameter 1");
-        assert_equals(0.0, result.get_parameter_values()[2], 1.0E-06, "test minimize cigar: parameter 2");
-        assert_equals(0.0, result.get_parameter_values()[3], 1.0E-06, "test minimize cigar: parameter 3");
-        assert_equals(0.0, result.get_parameter_values()[4], 1.0E-06, "test minimize cigar: parameter 4");
-        assert_equals(0.0, result.get_parameter_values()[5], 1.0E-06, "test minimize cigar: parameter 5");
-        assert_equals(0.0, result.get_parameter_values()[6], 1.0E-06, "test minimize cigar: parameter 6");
-        assert_equals(0.0, result.get_parameter_values()[7], 1.0E-06, "test minimize cigar: parameter 7");
-        assert_equals(0.0, result.get_parameter_values()[8], 1.0E-06, "test minimize cigar: parameter 8");
-        assert_equals(0.0, result.get_parameter_values()[9], 1.0E-06, "test minimize cigar: parameter 9");
-    }
-
-    void test_minimize_rosenbrock() {
-        using especia::No_Constraint;
-        using especia::No_Tracing;
-
-        const valarray<R_type> x(-1.0, 10);
-        const valarray<R_type> d(1.0, 10);
-        const R_type s = 1.0;
-
-        const Optimizer optimizer = builder.build();
-        const Optimizer::Result result = optimizer.minimize(rosenbrock, x, d, s, No_Constraint<>(), No_Tracing<>());
-
-        assert_true(result.is_optimized(), "test minimize rosenbrock: optimized");
-        assert_equals(0.0, result.get_fitness(), 1.0E-10, "test minimize rosenbrock: fitness");
-        assert_equals(1.0, result.get_parameter_values()[0], 1.0E-06, "test minimize rosenbrock: parameter 0");
-        assert_equals(1.0, result.get_parameter_values()[1], 1.0E-06, "test minimize rosenbrock: parameter 1");
-        assert_equals(1.0, result.get_parameter_values()[2], 1.0E-06, "test minimize rosenbrock: parameter 2");
-        assert_equals(1.0, result.get_parameter_values()[3], 1.0E-06, "test minimize rosenbrock: parameter 3");
-        assert_equals(1.0, result.get_parameter_values()[4], 1.0E-06, "test minimize rosenbrock: parameter 4");
-        assert_equals(1.0, result.get_parameter_values()[5], 1.0E-06, "test minimize rosenbrock: parameter 5");
-        assert_equals(1.0, result.get_parameter_values()[6], 1.0E-06, "test minimize rosenbrock: parameter 6");
-        assert_equals(1.0, result.get_parameter_values()[7], 1.0E-06, "test minimize rosenbrock: parameter 7");
-        assert_equals(1.0, result.get_parameter_values()[8], 1.0E-06, "test minimize rosenbrock: parameter 8");
-        assert_equals(1.0, result.get_parameter_values()[9], 1.0E-06, "test minimize rosenbrock: parameter 9");
-    }
-
     static R_type sphere(const R_type x[], N_type n) {
         using especia::sq;
 
@@ -149,6 +65,93 @@ private:
         }
 
         return y;
+    }
+
+    void before() {
+        builder.with_problem_dimension(10).
+                with_population_size(40).
+                with_parent_number(10).
+                with_accuracy_goal(1.0E-06).
+                with_random_seed(31415);
+    }
+
+    void after() {
+        builder.with_defaults();
+    }
+
+    void test_minimize_sphere() {
+        using especia::No_Constraint;
+        using especia::No_Tracing;
+
+        const valarray<R_type> x(1.0, 10);
+        const valarray<R_type> d(1.0, 10);
+        const R_type s = 1.0;
+
+        const Optimizer optimizer = builder.with_stop_generation(200).build();
+        const Optimizer::Result result = optimizer.minimize(sphere, x, d, s, No_Constraint<>(), No_Tracing<>());
+
+        assert_true(result.is_optimized(), "test minimize sphere: optimized");
+        assert_equals(0.0, result.get_fitness(), 1.0E-10, "test minimize sphere: fitness");
+        assert_equals(0.0, result.get_parameter_values()[0], 1.0E-06, "test minimize sphere: parameter 0");
+        assert_equals(0.0, result.get_parameter_values()[1], 1.0E-06, "test minimize sphere: parameter 1");
+        assert_equals(0.0, result.get_parameter_values()[2], 1.0E-06, "test minimize sphere: parameter 2");
+        assert_equals(0.0, result.get_parameter_values()[3], 1.0E-06, "test minimize sphere: parameter 3");
+        assert_equals(0.0, result.get_parameter_values()[4], 1.0E-06, "test minimize sphere: parameter 4");
+        assert_equals(0.0, result.get_parameter_values()[5], 1.0E-06, "test minimize sphere: parameter 5");
+        assert_equals(0.0, result.get_parameter_values()[6], 1.0E-06, "test minimize sphere: parameter 6");
+        assert_equals(0.0, result.get_parameter_values()[7], 1.0E-06, "test minimize sphere: parameter 7");
+        assert_equals(0.0, result.get_parameter_values()[8], 1.0E-06, "test minimize sphere: parameter 8");
+        assert_equals(0.0, result.get_parameter_values()[9], 1.0E-06, "test minimize sphere: parameter 9");
+    }
+
+    void test_minimize_cigar() {
+        using especia::No_Constraint;
+        using especia::No_Tracing;
+
+        const valarray<R_type> x(1.0, 10);
+        const valarray<R_type> d(1.0, 10);
+        const R_type s = 1.0;
+
+        const Optimizer optimizer = builder.with_stop_generation(400).build();
+        const Optimizer::Result result = optimizer.minimize(cigar, x, d, s, No_Constraint<>(), No_Tracing<>());
+
+        assert_true(result.is_optimized(), "test minimize cigar: optimized");
+        assert_equals(0.0, result.get_fitness(), 1.0E-10, "test minimize cigar: fitness");
+        assert_equals(0.0, result.get_parameter_values()[0], 1.0E-06, "test minimize cigar: parameter 0");
+        assert_equals(0.0, result.get_parameter_values()[1], 1.0E-06, "test minimize cigar: parameter 1");
+        assert_equals(0.0, result.get_parameter_values()[2], 1.0E-06, "test minimize cigar: parameter 2");
+        assert_equals(0.0, result.get_parameter_values()[3], 1.0E-06, "test minimize cigar: parameter 3");
+        assert_equals(0.0, result.get_parameter_values()[4], 1.0E-06, "test minimize cigar: parameter 4");
+        assert_equals(0.0, result.get_parameter_values()[5], 1.0E-06, "test minimize cigar: parameter 5");
+        assert_equals(0.0, result.get_parameter_values()[6], 1.0E-06, "test minimize cigar: parameter 6");
+        assert_equals(0.0, result.get_parameter_values()[7], 1.0E-06, "test minimize cigar: parameter 7");
+        assert_equals(0.0, result.get_parameter_values()[8], 1.0E-06, "test minimize cigar: parameter 8");
+        assert_equals(0.0, result.get_parameter_values()[9], 1.0E-06, "test minimize cigar: parameter 9");
+    }
+
+    void test_minimize_rosenbrock() {
+        using especia::No_Constraint;
+        using especia::No_Tracing;
+
+        const valarray<R_type> x(0.0, 10);
+        const valarray<R_type> d(0.1, 10);
+        const R_type s = 1.0;
+
+        const Optimizer optimizer = builder.with_stop_generation(400).build();
+        const Optimizer::Result result = optimizer.minimize(rosenbrock, x, d, s, No_Constraint<>(), No_Tracing<>());
+
+        assert_true(result.is_optimized(), "test minimize rosenbrock: optimized");
+        assert_equals(0.0, result.get_fitness(), 1.0E-10, "test minimize rosenbrock: fitness");
+        assert_equals(1.0, result.get_parameter_values()[0], 1.0E-06, "test minimize Rosenbrock: parameter 0");
+        assert_equals(1.0, result.get_parameter_values()[1], 1.0E-06, "test minimize Rosenbrock: parameter 1");
+        assert_equals(1.0, result.get_parameter_values()[2], 1.0E-06, "test minimize Rosenbrock: parameter 2");
+        assert_equals(1.0, result.get_parameter_values()[3], 1.0E-06, "test minimize Rosenbrock: parameter 3");
+        assert_equals(1.0, result.get_parameter_values()[4], 1.0E-06, "test minimize Rosenbrock: parameter 4");
+        assert_equals(1.0, result.get_parameter_values()[5], 1.0E-06, "test minimize Rosenbrock: parameter 5");
+        assert_equals(1.0, result.get_parameter_values()[6], 1.0E-06, "test minimize Rosenbrock: parameter 6");
+        assert_equals(1.0, result.get_parameter_values()[7], 1.0E-06, "test minimize Rosenbrock: parameter 7");
+        assert_equals(1.0, result.get_parameter_values()[8], 1.0E-06, "test minimize Rosenbrock: parameter 8");
+        assert_equals(1.0, result.get_parameter_values()[9], 1.0E-06, "test minimize Rosenbrock: parameter 9");
     }
 
     void run_all() {
