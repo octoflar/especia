abe3e7600a33bb9618b9f7bcdba7944edeb7892e
octoflar <octoflar@users.noreply.github.com>
2018-02-18 10:46:07 +0100

    update

--- src/test/cxx/core/profiles_test.cxx
+++ src/test/cxx/core/profiles_test.cxx
@@ -35,39 +35,39 @@ private:
     void test_equivalent_width_intergalactic_doppler() {
         using especia::Intergalactic_Doppler;
 
-        const real result = calculator.calculate(Intergalactic_Doppler());
+        const real result = calculator.calculate(Intergalactic_Doppler()) / real(1000.0);
 
         // <https://www.wolframalpha.com/input/?i=NIntegrate%5B1-Exp%5B-1%2F(0.5+Sqrt%5BPi%5D)+Exp%5B-(x%2F0.5)%5E2%5D%5D,+%7Bx,+-Infinity,+Infinity%7D%5D>
-        assert_equals(real(698.785), result, real(1.0E-03), "equivalent width (intergalactic Doppler)");
+        assert_equals(real(0.698785), result, real(1.0E-06), "equivalent width (intergalactic Doppler)");
     }
 
     void test_equivalent_width_many_multiplet() {
         using especia::Many_Multiplet;
 
-        const real result = calculator.calculate(Many_Multiplet());
+        const real result = calculator.calculate(Many_Multiplet()) / real(1000.0);
 
         // <https://www.wolframalpha.com/input/?i=NIntegrate%5B1-Exp%5B-1%2F(0.5+Sqrt%5BPi%5D)+Exp%5B-(x%2F0.5)%5E2%5D%5D,+%7Bx,+-Infinity,+Infinity%7D%5D>
-        assert_equals(real(698.785), result, real(1.0E-03), "equivalent width (many-multiplet)");
+        assert_equals(real(0.698785), result, real(1.0E-06), "equivalent width (many-multiplet)");
     }
 
     void test_equivalent_width_intergalactic_voigt() {
         using especia::Intergalactic_Voigt;
         using especia::Pseudo_Voigt;
 
-        const real result = calculator.calculate(Intergalactic_Voigt<Pseudo_Voigt>());
+        const real result = calculator.calculate(Intergalactic_Voigt<Pseudo_Voigt>()) / real(1000.0);
 
-        // 1000 NIntegrate[1 - Exp[-PDF[VoigtDistribution[0.5, 0.5/Sqrt[2]], x]], {x, -Infinity, Infinity}]
-        assert_equals(real(881.143), result, real(4.4), "equivalent width (intergalactic Voigt)");
+        // <https://www.wolframalpha.com/input/?i=NIntegrate%5B1+-+Exp%5B-PDF%5BVoigtDistribution%5B0.5,+0.5%2FSqrt%5B2%5D%5D,+x%5D%5D,+%7Bx,+-Infinity,+Infinity%7D%5D>
+        assert_equals(real(0.881143), result, real(3.5E-03), "equivalent width (intergalactic Voigt)");
     }
 
     void test_equivalent_width_intergalactic_voigt_extended() {
         using especia::Intergalactic_Voigt;
         using especia::Extended_Pseudo_Voigt;
 
-        const real result = calculator.calculate(Intergalactic_Voigt<Extended_Pseudo_Voigt>());
+        const real result = calculator.calculate(Intergalactic_Voigt<Extended_Pseudo_Voigt>()) / real(1000.0);
 
-        // 1000 NIntegrate[1 - Exp[-PDF[VoigtDistribution[0.5, 0.5/Sqrt[2]], x]], {x, -Infinity, Infinity}]
-        assert_equals(real(881.143), result, real(4.4), "equivalent width (intergalactic Voigt, extended)");
+        // <https://www.wolframalpha.com/input/?i=NIntegrate%5B1+-+Exp%5B-PDF%5BVoigtDistribution%5B0.5,+0.5%2FSqrt%5B2%5D%5D,+x%5D%5D,+%7Bx,+-Infinity,+Infinity%7D%5D>
+        assert_equals(real(0.881143), result, real(4.5E-03), "equivalent width (intergalactic Voigt, extended)");
     }
 
     void test_maximum_pseudo_voigt() {
