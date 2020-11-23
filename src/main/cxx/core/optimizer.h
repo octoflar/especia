bbac058913ea66ee16087d49e591a337ac79210e
octoflar <octoflar@users.noreply.github.com>
2020-11-24 00:45:37 +0100

    Use PCG: initialisation

--- src/main/cxx/core/optimizer.h
+++ src/main/cxx/core/optimizer.h
@@ -55,7 +55,7 @@ namespace especia {
          * @param[in] seed The seed.
          * @param[in] selector The sequence selector.
          */
-        explicit Pcg(const longword seed = 0x853c49e6748fea9bull, const longword selector = 0xda3e39cb94b95bdaull >> 1u) : inc((selector << 1u) | 1ull) {
+        explicit Pcg(const longword seed = 9600629759793949339ull, const longword selector = 7863035247680335341ul) : inc((selector << 1u) | 1ull) {
             state = 0ull;
             rand();
             state += seed;
