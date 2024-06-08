9753b73e0feb773a3d76c42e6683e3ed2024f764
R. Quast <octoflar@icloud.com>
2024-06-08 10:15:32 +0200

    Update test.cmake

--- src/main/cmake/test.cmake
+++ src/main/cmake/test.cmake
@@ -15,6 +15,7 @@ function(add_unit_test NAME)
             ${ARGN}
             ${TEST}/cxx/unittest.h)
     target_compile_options(${NAME} PRIVATE --coverage)
+    target_link_options(${NAME} PRIVATE --coverage)
     add_test(NAME ${NAME} COMMAND ${NAME})
     set_tests_properties(${NAME} PROPERTIES LABELS unit TIMEOUT 10)
 endfunction()
