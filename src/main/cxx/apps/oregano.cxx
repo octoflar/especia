9b3ad8726dac19334be7648ad39cea56e43f3965
R. Quast <octoflar@icloud.com>
2024-06-18 00:11:00 +0200

    Update oregano.cxx

--- src/main/cxx/apps/oregano.cxx
+++ src/main/cxx/apps/oregano.cxx
@@ -11,7 +11,7 @@
 
 using namespace std;
 
-/// Especia to analyse intergalactic metal and damped H I, He I, II lines.
+/// Especia to analyse intergalactic metal and non-damped H I, He I, II lines.
 ///
 /// @param argc The number of command line arguments.
 /// @param argc The number of command line arguments.
